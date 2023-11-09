c0099ac8615c8a33a515d210c45419eb01239c36
Hyeeun <ella.h.park@fau.de>
2023-11-09 16:26:48 +0100

    clean test code

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,41 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import pickRandomInteger, choiceOperator, calculateFormula
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def TestPickRandomInteger(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = pickRandomInteger(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def TestChoiceOperator(self):
+        # Test if ramdom operator generated is in the operator list (+, -, *)
+        operator_list = ['+', '-', '*']
+        for _ in range(1000):
+             operator = choiceOperator()
+             self.assertTrue(operator in operator_list)
 
-    def test_function_C(self):
+    def TestCalculateFormula(self):
+            # Test if the calculation of the test cases is correct
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (4, 2, '*', '4 * 2', 8),
+                (9, 5, '-', '9 - 5', 4),
+                (6, 4, '*', '6 * 4', 24),
+                (9, 1, '*', '9 * 1', 9),
+                (4, 4, '-', '4 - 2', 0),
+                (5, 1, '+', '4 * 2', 6),
+                (7, 3, '*', '7 * 3', 21),
+                (7, 2, '-', '7 * 2', 5),
+                (8, 2, '+', '8 + 2', 10)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                 self.assertTrue(expected_problem, expected_answer == calculateFormula(num1, num2, operator))
 
 if __name__ == "__main__":
     unittest.main()
